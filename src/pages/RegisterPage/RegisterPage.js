5cbe21b08139426a9d25da1d40e76d6a804fe93d
searchmoon <moonding901212@gmail.com>
2022-02-16 14:52:26 +0900

    clayful 설정 및 axios 다운로드

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+    <div className="">
+      <h1>회원가입</h1>
+      <form>
+        <input type="email" name="email" placeholder="Apple ID" value="" />
+        <input type="password" name="email" placeholder="암호" value="" />
+        <button type="submit">회원가입</button>
+        <Link to="login" style={{ color: "gray", textDecoration: "none"}}>
+          {" "}
+          이미 Apple ID가 있다면? 지금 로그인
+          </Link>
+      </form>
+    </div>
   )
 }
 

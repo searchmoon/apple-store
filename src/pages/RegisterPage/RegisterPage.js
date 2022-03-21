d520c52a149b69ba74a8e921e3ee306f354612b7
searchmoon <moonding901212@gmail.com>
2022-03-22 00:44:06 +0900

    useState 추가

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,18 +1,22 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function RegisterPage() {
+  const [ email, setEmail ] = useState('');
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  }
   return (
     <div className="auth-wrapper">
       <h1>회원가입</h1>
       <form>
-        <input type="email" name="email" placeholder="Apple ID" value="" />
-        <input type="password" name="email" placeholder="암호" value="" />
+        <input onChange={handleEmailChange} placeholder="Apple ID" type="email" name="email" value={email} />
+        <input placeholder="암호" type="password" name="email" value="" />
         <button type="submit">회원가입</button>
-        <Link to="login" style={{ color: "gray", textDecoration: "none"}}>
+        <Link to="login" style={{ color: "gray", textDecoration: "none" }}>
           {" "}
           이미 Apple ID가 있다면? 지금 로그인
-          </Link>
+        </Link>
       </form>
     </div>
   )

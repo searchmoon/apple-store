afe5905d86c9e6e7ac8a4463705b9fb5d42d7fd0
searchmoon <moonding901212@gmail.com>
2022-03-22 15:30:19 +0900

    chapter.3-5 로그인페이지 구현

--- src/pages/RegisterPage/RegisterPage.js
+++ src/pages/RegisterPage/RegisterPage.js
@@ -1,17 +1,57 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import clayful from "clayful/client-js";
 
 function RegisterPage() {
-  const [ email, setEmail ] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
-  }
+  };
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    var Customer = clayful.Customer;
+
+    var payload = {
+      email: email,
+      password: password,
+      //email: email처럼 key와 value가 같을 경우 email만으로 생략해도 된다.
+    };
+console.log("payload", payload)
+    Customer.createMe(payload, function (err, result) {
+      if (err) {
+        // Error case
+        console.log(err.code);
+      }
+
+      navigate("/login");
+    });
+  };
+
   return (
     <div className="auth-wrapper">
       <h1>회원가입</h1>
-      <form>
-        <input onChange={handleEmailChange} placeholder="Apple ID" type="email" name="email" value={email} />
-        <input placeholder="암호" type="password" name="email" value="" />
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleEmailChange}
+          placeholder="Apple ID"
+          type="email"
+          name="email"
+          value={email}
+        />
+        <input
+          onChange={handlePasswordChange}
+          placeholder="암호"
+          type="password"
+          name="email"
+          value={password}
+        />
         <button type="submit">회원가입</button>
         <Link to="login" style={{ color: "gray", textDecoration: "none" }}>
           {" "}
@@ -19,7 +59,7 @@ function RegisterPage() {
         </Link>
       </form>
     </div>
-  )
+  );
 }
 
 export default RegisterPage;

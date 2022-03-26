e40aa396d8c5990014b952ba3d95726cffab0503
searchmoon <moonding901212@gmail.com>
2022-03-26 13:20:32 +0900

    chapter.6-2. 장바구니 상품 나열

--- src/pages/CartPage/CartPage.js
+++ src/pages/CartPage/CartPage.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import clayful from "clayful/client-js";
 import { useNavigate } from "react-router-dom";
+import "./CartPage.css";
+import CartItem from "./Sections/CartItem";
 
 function CartPage() {
   const navigate = useNavigate();
@@ -26,17 +28,31 @@ function CartPage() {
   const items = cart.items;
   return (
     <div className="pageWrapper">
-      <div className="shipping-cart">
+      <div className="shopping-cart">
         <h1 className="title">장바구니</h1>
-        <div className="shopping-cart-body" style={{ minHeight: 100 }}></div>
+        <div className="shopping-cart-body" style={ { minHeight: 100 } }>
+          { items && items.length > 0 ? (
+            items.map((item, index) => {
+              return <CartItem
+                key={ item._id }
+                item={ item }
+                index={ index }
+              />
+            })
+          ): (
+              <p style={{ textAlign: "center", marginTop: "2rem"}}>
+                카트에 상품이 하나도 없습니다.
+              </p>
+          )}
+        </div>
 
-        {items && items.length > 0 && (
+        { items && items.length > 0 && (
           <div className="bottom">
             <span className="total-price">
-              총 금액 : ₩ {cart.total?.amout.raw}
+              총 금액: ₩ {cart.total?.amount.raw}
             </span>
-            <button
-              style={{ float: "right", padding: "4px 8px" }}
+            <button 
+              style={ { float: "right", padding: "4px 8px" } }
               onClick={() => navigate("/payment")}
             >
               결제
@@ -45,7 +61,7 @@ function CartPage() {
         )}
       </div>
     </div>
-  );
+  )
 }
 
 export default CartPage;
